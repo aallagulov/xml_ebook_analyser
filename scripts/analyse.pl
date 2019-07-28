c2fa101b440defe133af825525a261770050240d
Anvar Allagulov <payable@yandex.ru>
2019-07-28 14:24:49 +0300

    add <a href counting to the script

--- scripts/analyse.pl
+++ scripts/analyse.pl
@@ -18,16 +18,15 @@ GetOptions(
 $file or pod2usage("Please specify (-f|--file) to work with\n");
 
 my $dom = XML::LibXML->load_xml(location => $file);
-my $root   = $dom->getDocumentElement();
-my $xpc    = XML::LibXML::XPathContext->new($root);
+my $full_text = $dom->textContent();
 
-my $full_text = '';
-for my $nodes ($xpc->findnodes('/')) {
-	$full_text .= $nodes->textContent();
-}
 my ($non_space_text, $normalized_text) = ($full_text, $full_text);
 $non_space_text =~ s/\s//g;
 say length($non_space_text);
 
 my @normalized_lines = map {s/^\s+|\s+$//;$_} grep {/\w/} split("\n", $normalized_text);
 say length(join("",@normalized_lines)); 
+
+my $href_nodelist = $dom->getElementsByTagName("a");
+say $href_nodelist->size();
+
