e754c717cdeccd5322c53464a72919f7399cd560
Anvar Allagulov <payable@yandex.ru>
2019-07-28 14:08:25 +0300

    return count of text symbols w/ and w/o spaces

--- scripts/analyse.pl
+++ scripts/analyse.pl
@@ -7,6 +7,7 @@ use feature 'say';
 use Getopt::Long;
 use Pod::Usage;
 use XML::LibXML;
+use XML::LibXML::XPathContext;
 use Data::Dumper;
 
 my ($file);
@@ -17,17 +18,16 @@ GetOptions(
 $file or pod2usage("Please specify (-f|--file) to work with\n");
 
 my $dom = XML::LibXML->load_xml(location => $file);
-# my $nodes = $dom->findnodes('//FictionBook ');  
-# print $nodes->size() . "\n";
+my $root   = $dom->getDocumentElement();
+my $xpc    = XML::LibXML::XPathContext->new($root);
 
-# for my $node ($nodes->get_nodelist()) {
-#     print $node->nodeName . "\n";
-# }
-my $text = '';
-for my $nodes ($dom->findnodes('/')) {
-	say ref($nodes);
-	$text .= $nodes->to_literal();
+my $full_text = '';
+for my $nodes ($xpc->findnodes('/')) {
+	$full_text .= $nodes->textContent();
 }
-say $text;
-$text =~ s/\s//g;
-say $text;
+my ($non_space_text, $normalized_text) = ($full_text, $full_text);
+$non_space_text =~ s/\s//g;
+say length($non_space_text);
+
+my @normalized_lines = map {s/^\s+|\s+$//;$_} grep {/\w/} split("\n", $normalized_text);
+say length(join("",@normalized_lines)); 
